38e2cd034f0d2156c75f8c3b96465fe268883fdf
Hassan Habib <hassanhabib@live.com>
2025-04-27 07:57:27 -0700

    POC: Initialize Project

---
+++ IOfflineAIModel.cs
@@ -0,0 +1,8 @@
+namespace OfflineAI
+{
+    public interface IOfflineAIModel
+    {
+        string GenerateText(string prompt, int maxTokens = 100);
+        Task<string> GenerateTextAsync(string prompt, int maxTokens = 100);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using OfflineAI;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! ask me anything!");
+
+            while(true)
+            {
+                string question = Console.ReadLine();
+
+                var llama = new LlamaCppModel(
+                    "models/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf", 
+                    "models/llama-cli.exe");
+                var output = llama.GenerateText(
+                    question);
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
